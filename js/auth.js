63c8235e0e805cd762bec11e48ad6002b0cee1e3
Edmond Chen <edmondchen@Edmonds-MacBook-Pro-2.local>
2022-08-08 09:07:49 +0800

    TBT-33 Added logout button

--- js/auth.js
+++ js/auth.js
@@ -19,10 +19,13 @@ function getUser() {
 }
 
 function validateCognitoUser() {
+    // Validates that the app is logged in. Disregards whether session has expired or not
     var cognitoUser = getUser();
     
 	//window.onload = function() {
     if (cognitoUser != null) {
+        // This fetches new session for the current user stored in localstorage no matter whether session has expired or not
+        // https://stackoverflow.com/questions/42002953/how-to-remember-autorefresh-login-token-when-using-amazon-web-services-cognito
         let success = cognitoUser.getSession(function(err, session) {
             if (err) {
                 alert(err);
@@ -30,6 +33,7 @@ function validateCognitoUser() {
             }
             console.log('session validity: ' + session.isValid());
             
+            // Get user attributes for info
 			cognitoUser.getUserAttributes(function(err, result) {
 				if (err) {
 					console.log(err);
