52b69f32e9732f56ed687fbbd2d760c2bf898964
Edmond Chen Si Jie <edmond.chen@changiairport.com>
2022-08-11 15:53:23 +0800

    TBT-37 Added wake lock

--- js/utils.js
+++ js/utils.js
@@ -22,8 +22,6 @@ const goToPage = async function (routeKey) {
 const busPlateStorageKey = "bus-plate";
 
 const setBusPlate = function (plateNumber) {
-    // Set bus plate text in HTML div and save to local storage
-    document.getElementById("bus-plate").textContent = plateNumber;
     localStorage.setItem(busPlateStorageKey, plateNumber);
 }
 
@@ -44,4 +42,46 @@ function getCurrentISOTime() {
   console.log("timestamp: ", timestamp)
   return timestamp
 }
-export { setInnerHTMLWithJS, goToPage, setBusPlate, clearBusPlate, getBusPlate, getCurrentISOTime };
+
+async function setScreenWakeLock() {
+  // Checks whether browser has wake-lock function and triggers wakelock if available
+  const wakeLockDiv = document.getElementById("wake-lock-alert");
+  var isSupported = false;
+
+  // Check browser compatibility 
+  if ('wakeLock' in navigator) {
+    console.log("Wake lock supported");
+    isSupported = true;
+  } else {
+    console.log("Wake lock not supported");
+    wakeLockDiv.innerHTML = `
+      <div class="alert alert-danger text-center">This browser does not support Wake-lock and may result in the app going to sleep. <br>Please use a different browser (Recommended: Chrome).</div>
+    `;
+  }
+  
+  if (isSupported) {
+    // Enable wake-lock
+    let wakeLock = null;
+
+    // Request a wake lock
+    try {
+      wakeLock = await navigator.wakeLock.request('screen');
+      console.log('Wake Lock enabled!');
+    } catch (err) {
+      console.log("Error occurred while enabling wake-lock: ", err)
+      // The Wake Lock request has failed - usually system related, such as battery.
+      wakeLockDiv.innerHTML = `
+        <div class="alert alert-danger text-center">An error occurred when enabling wake-lock. Please make sure the device is charging. </div>
+      `;
+    }
+
+    // Create listener to re-activate wake-lock if visibility of page changed (inactive to active) and wake-lock was released for some reason
+    document.addEventListener('visibilitychange', async () => {
+      if (wakeLock !== null && document.visibilityState === 'visible') {
+        console.log("Re-activating wakelock");
+        wakeLock = await navigator.wakeLock.request('screen');
+      }
+    });
+  }
+}
+export { setInnerHTMLWithJS, goToPage, setBusPlate, clearBusPlate, getBusPlate, getCurrentISOTime, setScreenWakeLock };
